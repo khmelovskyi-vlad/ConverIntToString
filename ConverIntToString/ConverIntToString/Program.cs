d8d9c4cd0ae38b80375e9ba50cb362b87a964ebe
vladhmelevskiy1234765 <vladhmelevskiy1234765@gmail.com>
2019-06-13 19:23:28 +0300

    initial

---
+++ ConverIntToString/ConverIntToString/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverIntToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "123";
+            double doubleTest = NewMethod(test);
+            Console.WriteLine(doubleTest);
+            Console.ReadKey();
+        }
+
+        private static double NewMethod(string text)
+        {
+            int convToInt;
+            double conv = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                convToInt = text[i] - 48;
+                if (convToInt >= 0 && convToInt <= 9)
+                {
+                    conv = conv + convToInt * Math.Pow(10, text.Length - i - 1);
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            Console.WriteLine(conv);
+            return conv;
+        }
+    }
+}
